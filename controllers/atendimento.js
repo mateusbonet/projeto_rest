c9f4ef434a087c7d66ba521c8fdaebb8570ff058
Mateus Bonet <mateusbonet@yahoo.com.br>
2021-12-05 13:24:01 -0300

    Doc: Realizado ajustes na estrutura do projeto, ainda da pra melhorar.

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,12 +1,43 @@
+const Atendimento = require('../models/atendimentos');
+
 module.exports  = app => {
 
   app.get('/atendimentos', (req, res) => {
-    res.send('Rota de atendimentos!');
+    
+    Atendimento.listar(res);
+
+  });
+
+  app.get('/atendimento/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Atendimento.buscarId(id, res);
+
   });
 
   app.post('/atendimentos', (req, res) => {
-    console.log(req.body);
-    res.send('Você está na rota de atendimentos!');
+    
+    const atendimento = req.body;
+
+    Atendimento.adicionar(atendimento, res);
+    
   })
+
+  app.patch('/atendimentos/:id', (req, res) => {
+    
+    const id = parseInt(req.params.id);
+    const values = req.body;
+
+    Atendimento.alterar(id, values, res);
+
+  });
+
+  app.delete('/atendimentos/:id', (req, res) => {
+    
+    const id = parseInt(req.params.id);
+
+    Atendimento.deletar(id, res);
+
+  });
 
 }
---
+++ models/atendimentos.js
@@ -0,0 +1,116 @@
+const moment = require('moment');
+const conexao = require('../database/conection');
+
+class Atendimento {
+
+  adicionar(atendimento, res) {
+
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+    // Validação de data e validação do tamanho do cliente
+    const dataValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteValido = atendimento.cliente.length >= 5;
+
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataValida,
+        mensagem: 'Data deve ser maior que a atual'
+      },
+      {
+        nome: 'cliente',
+        valido: clienteValido,
+        mensagem: 'Cliente deve ter ao menos 5 caracteres'
+      }
+    ];
+
+    const errors = validacoes.filter ( campo => !campo.valido );
+    const existemErrors = errors.length;
+
+    if (existemErrors){
+      res.status(400).send(errors);
+    } else {
+
+      const atendimentoDatado = { ...atendimento, data, dataCriacao };
+
+      const sql ='INSERT INTO Atendimentos SET ? ';
+
+      conexao.query(sql, atendimentoDatado, (erro, results) => {
+        if(erro){
+         res.status(400).json(erro);
+        }else{
+          res.status(201).json( atendimento );
+        }
+      })
+
+    }
+
+  }
+
+  listar (res) {
+
+    const sql = 'SELECT * FROM Atendimentos';
+
+    conexao.query(sql, (erro, results) => {
+
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(results);
+      }
+    })
+
+  }
+
+  buscarId (id, res) {
+
+    const sql = `SELECT * FROM Atendimentos where id = ${id} `;
+
+    conexao.query(sql, (erro, results) => {
+
+      const resultado = results[0];
+      
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultado);
+      }
+    })
+  }
+
+  alterar (id, values, res) {
+
+    if (values.data) {
+      values.data = moment(values.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    }
+
+    const sql = `UPDATE Atendimentos SET ? WHERE id = ? `;
+
+    conexao.query(sql, [values, id], (erro, results) => {
+      
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ ... values, id });
+      }
+    })
+  }
+
+  deletar (id, res) {
+
+    const sql = `DELETE FROM Atendimentos WHERE id = ? `;
+
+    conexao.query(sql, id, (erro, results) => {
+      
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ id });
+      }
+    })
+  }
+
+}
+
+module.exports = new Atendimento;
